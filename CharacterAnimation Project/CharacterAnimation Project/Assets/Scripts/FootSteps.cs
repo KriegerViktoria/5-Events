74e7cc8471230080ad408187b4a60f26d578cb31
sasc0076 <sasc0076@users.noreply.github.com>
2020-05-08 01:18:02 +0200

    07.05 Scriptable Objects + neuer Hintergrund
    > Hintergrund auf langes Bild (ist aber noch schlechte auflösung)
    > Scriptable Objects kann man machen mit Rechtsclick -> Create -> ScriptableObjects -> Inventory Item (aber sie haben noch keinen Nutzen)
    >angefangen an Inventar script --> Tutorial in Discord oder https://www.youtube.com/watch?v=_IqTeruf3-s

---
+++ CharacterAnimation Project/CharacterAnimation Project/Assets/Scripts/FootSteps.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField]
+
+    private AudioSource audioSource;
+    public AudioClip[] list;
+    private new AudioSource audio;
+    CharacterController characterController;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    void Start()
+    {
+        
+        audio = GetComponent<AudioSource>();
+        characterController = GetComponent<CharacterController>();
+
+        list = new AudioClip[]
+        {
+        (AudioClip)Resources.Load("Grass Steps 1"),
+        (AudioClip)Resources.Load("Grass Steps 2")
+        };
+
+    }
+    private void Step()
+    {
+        audio.PlayOneShot(list[Random.Range(0, list.Length)]);
+    }
+
+
+   
+
+   
+}
---
+++ CharacterAnimation Project/CharacterAnimation Project/Assets/Scripts/PointandClickScript.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointandClickScript : MonoBehaviour
+{
+    public Vector3 mousePos;
+    public Camera mainCam;
+    public Vector3 mousePosWorld;
+    public Vector2 mousePos2d;
+    RaycastHit2D hit;
+
+    public GameObject player;
+
+    public Vector2 targetPos;
+    public float speed;
+    public bool isMoving;
+    
+
+    public Animator MCAnimator;
+
+    //für Step Audio
+    //public AudioClip[] list;
+   // AudioSource audio;
+    //CharacterController characterController;
+    //int number;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //für Step Audio
+       // audio = GetComponent<AudioSource>();
+        //characterController = GetComponent<CharacterController>();
+
+        //list = new AudioClip[]
+       // {
+       // (AudioClip)Resources.Load("Grass 1"),
+       // (AudioClip)Resources.Load("Grass 2")
+       // };
+
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetMouseButtonDown(0))
+        {
+          // MousePosition muss umgewandelt werden, da sonst die Kooridinaten der Screenspace genommen wird
+          // Koordinaten würden sich wiederholen! 
+          
+            mousePos = Input.mousePosition;
+           
+                        print("Screen Space: " + mousePos);
+            mousePosWorld = mainCam.ScreenToWorldPoint(mousePos);
+                        print("World Space: " + mousePosWorld);
+
+            //Muss MousePosWorld in einen Vector2 umwandeln, da Physics2D/Raycast nur damit arbeiten kann
+            mousePos2d = new Vector2(mousePosWorld.x, mousePosWorld.y);
+
+            // RayCast
+            hit = Physics2D.Raycast(mousePos2d, Vector2.zero);
+
+            //Überprüfe ob der Ray einen Collider trifft
+            if (hit.collider != null)
+            {
+                //Name des Objektes ausgeben
+                print("Hit! " + hit.collider.gameObject.name);
+
+                if (hit.collider.gameObject.tag == "Ground")
+                {
+                    //Koordinaten werden in einer variable gespeichert, die man dann zum bewegen braucht
+                    targetPos = hit.point;
+
+                    isMoving = true;
+                  
+
+                    //Überprüfen ob Sprite-Flip notwendig ist
+                    CheckSpriteFlip();
+                }
+                else if(hit.collider.gameObject.tag == "Collectable")
+                {
+                    //Einsammeln und im Skript speichern --> um es als Schlüssel zu verwenden
+                    hit.collider.gameObject.SetActive(false);
+                }
+            }
+          
+
+        }
+        //für Step Audio
+
+       // if (isMoving)
+        //{
+        //    audio.PlayOneShot(list[Random.Range(0, list.Length)]);
+            
+       // }
+    }
+
+    void CheckSpriteFlip()
+    {
+        //Spieler spiegeln damit Blickrichtung stimmt
+    
+
+        if (player.transform.position.x > targetPos.x)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+  
+    //Fixed update, damit sich der Character überall gleich bewegt und nicht frame bezogen
+    private void FixedUpdate()
+    {
+        //Spieler an Mausklick laufen lassen
+        if (isMoving)
+        {
+            player.transform.position = Vector3.MoveTowards(player.transform.position, targetPos, speed);
+            MCAnimator.SetBool("Walk", true);
+
+            if(player.transform.position.x == targetPos.x || player.transform.position.y == targetPos.y)
+            {
+                isMoving = false;
+                MCAnimator.SetBool("Walk", false);
+            }
+        }
+      }
+
+
+
+     
+
+
+                              
+    }
