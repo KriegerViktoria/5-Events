321f7e7eff4d92f1e2367a8cc9e8a819b5d23573
sasc0076 <sasc0076@users.noreply.github.com>
2020-05-12 20:57:48 +0200

    audiofix
    > Audio geht jetzt!
    > UI sollte jtzt überall gleich bleiben

--- CharacterAnimation Project/CharacterAnimation Project/Assets/Scripts/FootSteps.cs
+++ CharacterAnimation Project/CharacterAnimation Project/Assets/Scripts/FootSteps.cs
@@ -6,7 +6,6 @@ public class FootSteps : MonoBehaviour
 
     private AudioSource audioSource;
     public AudioClip[] list;
-    private new AudioSource audio;
     CharacterController characterController;
 
     private void Awake()
@@ -18,19 +17,11 @@ public class FootSteps : MonoBehaviour
     void Start()
     {
         
-        audio = GetComponent<AudioSource>();
-        characterController = GetComponent<CharacterController>();
-
-        list = new AudioClip[]
-        {
-        (AudioClip)Resources.Load("Grass Steps 1"),
-        (AudioClip)Resources.Load("Grass Steps 2")
-        };
 
     }
     private void Step()
     {
-        audio.PlayOneShot(list[Random.Range(0, list.Length)]);
+        audioSource.PlayOneShot(list[Random.Range(0, list.Length)]);
     }
 
 
