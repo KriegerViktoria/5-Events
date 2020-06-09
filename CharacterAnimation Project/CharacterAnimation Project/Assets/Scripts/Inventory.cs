5c10ae49b64aaa28ac5f939d9ffd5aa12a18febd
KriegerViktoria <64832930+KriegerViktoria@users.noreply.github.com>
2020-06-09 16:51:55 +0200

    Korrekturen Stock
    AAAAAAAAAA

--- CharacterAnimation Project/CharacterAnimation Project/Assets/Scripts/Inventory.cs
+++ CharacterAnimation Project/CharacterAnimation Project/Assets/Scripts/Inventory.cs
@@ -8,19 +8,19 @@ public class Inventory : MonoBehaviour
     public int[] items;
     public GameObject[] slots;
 
+    public bool firstClick = true;
     public Item lastItem;
-
-    private Inventory inventory;
     
 
 
     public void CheckCombination(Item CurrentItem)
     {
         print("Checking....");
-        if (lastItem == null)
+        if (firstClick)
         {
             lastItem = CurrentItem;
             print("Item:" + lastItem);
+            firstClick = false;
         }
         else
         {
@@ -31,15 +31,16 @@ public class Inventory : MonoBehaviour
                 if (CurrentItem.Combinations[j].ItemB == lastItem)
                 {
                     print("Combination found" + CurrentItem.Combinations[j].Result);
-                    for (int i = 0; i < inventory.items.Length; i++)
+                    for (int i = 0; i < items.Length; i++)
                     //test 06.06.2020
                     {
-                        if (inventory.items[i] == 0)
+                        if (items[i] == 0)
                         {   // check whether the slot is EMPTY
                             // Instantiate(effect, transform.position, Quaternion.identity);
-                            inventory.items[i] = 1; // makes sure that the slot is now considered FULL
-                            Instantiate(lastItem.Combinations[j].Result.inventoryPrefab, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
-
+                            items[i] = 1; // makes sure that the slot is now considered FULL
+                            Instantiate(lastItem.Combinations[j].Result.inventoryPrefab, slots[i].transform, false); // spawn the button so that the player can interact with it
+                            firstClick = true;
+                            lastItem = null;
                            // RectTransform newRectTransform = newGameObject.GetComponent<RectTransform>();
                             //newRectTransform.localPosition = Vector3.zero;
 
