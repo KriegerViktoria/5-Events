300b712ac82593ecf4703d8674e3a20b383eb230
KriegerViktoria <64832930+KriegerViktoria@users.noreply.github.com>
2020-06-19 18:17:14 +0200

    19.06. ERSTE CUTSCENE YEET
    - oben stehts
    - \.u./
    - die walk Animation hat es leider bei den neuen transitions verhauen im sorryyyyyy

---
+++ 5 Events - 13.6.20/Assets/Scripts/IdleBool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleBool : MonoBehaviour
+{
+    public Animator MCAnimator;
+
+    public void CutsceneStart()
+    {
+        MCAnimator.SetBool("Idle", false);
+        print("idle false");
+    }
+
+    public void CutSceneEnd()
+    {
+        MCAnimator.SetBool("Idle", true);
+    }
+}
