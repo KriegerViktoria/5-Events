dda2e693e0b4eb277e3c333171128cf15ce8d9de
KriegerViktoria <64832930+KriegerViktoria@users.noreply.github.com>
2020-06-24 15:30:36 +0200

    24.06. Cutscene 3+4

---
+++ 5 Events - 13.6.20/Assets/Scripts/Grandmother_AnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grandmother_AnimationController : MonoBehaviour
+{
+    public Animator GMAnimator;
+
+
+    public void CutSceneAnim()
+    {
+        GMAnimator.SetBool("Idle", false);
+
+    }
+
+    public void IdleAnim()
+    {
+        GMAnimator.SetBool("Idle", true);
+    }
+
+    
+}
+
+
---
+++ 5 Events - 13.6.20/Assets/Scripts/PlayCutscene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class PlayCutscene : MonoBehaviour
+{
+    public PlayableDirector Cutscene;
+    public GameObject Trigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            Cutscene.Play();
+            Trigger.SetActive(false);
+        }
+    }
+
+}
